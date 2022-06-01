d72debbbe97a9acd1c1090825365a030a164db29
Shahid <shahid.alam57@gmail.com>
2022-06-01 19:15:08 +0530

    added validations for login

--- src/Forms/SignUpForm.jsx
+++ src/Forms/SignUpForm.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const SignUpForm = () => {
   let inputStyles='h-12 rounded-lg border w-full p-2 border border-1 border-[#dbdde0]'
+  let [email,setEmail] = useState('')
+  let [password,setPassword] = useState('')
+  const [fName, setFName] = useState('')
+  const [lName, setLName] = useState('')
+
+
   return (
-    <form action="" className='flex flex-col justify-around h-[60%]'>
+    <form action="" className='flex flex-col justify-around h-[60%]' >
         <div className='flex justify-between'>
-            <input type="text" name="fname" id="fname" placeholder='First name' className={inputStyles} />
-            <input type="text" name="lname" id="lname" placeholder='Last name' className={inputStyles} />
+            <input type="text" name="fname" id="fname" placeholder='First name' required className={inputStyles} onChange={(e)=>setFName(e.target.value)} />
+            <input type="text" name="lname" id="lname" placeholder='Last name' required className={inputStyles}  />
         </div>
-        <input type="email" name="email" id="email" className={inputStyles} placeholder='Email' />
-        <input type="password" name="password" id="password" className={inputStyles} placeholder='Password' />
+        <input type="email" name="email" id="email" required className={inputStyles} placeholder='Email' onChange={(e)=>setLName(e.target.value)} />
+        <input type="password" name="password" id="password" required className={inputStyles} placeholder='Password' onChange={(e)=>setPassword(e.target.value)} />
         <p> By creating an account, you agreeing to our <span className='font-bold'>Privacy Policy</span> , and <span className='font-bold'>Electronics Communication Policy</span>.</p>
        
         <button type="submit"
         className='w-full text-white bg-[#194BFB] px-1.5 py-3 rounded font-[800] tracking-[0.2px] capitalize'
-        onClick={(e)=>e.preventDefault()}
         >
         Sign Up</button>
     </form>
